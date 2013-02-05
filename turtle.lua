8c825451ca505e16e1be45cccd29ccf2a3c0ec07
Brekkjern <nils.kristian.brekke@gmail.com>
2013-02-05 12:15:08 +0100

    Changed a crapton of stuff on turtleMove, turtleTurn, placeBlock. Removed a crapton of unneeded functions.

--- turtle.lua
+++ turtle.lua
@@ -1,7 +1,13 @@
 local directions = {
-    x = { pos = "north", neg = "south"},
-    y = { pos = "west", neg= "east"},
-    z = { pos = "up", neg = "down"}
+  x = { pos = "north", neg = "south"},
+  y = { pos = "west", neg= "east"},
+  z = { pos = "up", neg = "down"}
+}
+local numericDirections = {
+  north = 0,
+  east = 1,
+  south = 2,
+  west = 3
 }
 
 -- Enumeration to store the the different types of message that can be written
@@ -20,8 +26,8 @@ local facing = 0
 
 -- Inventory.
 local inventory = {
-  list = {},      -- List of slot usage
-  left = {},
+  count = {},      -- List of slot usage
+  space = {},
   scrap = {},     -- List of scrap block slots
   empty = 0,      -- Empty inventory slots
   full = 0,       -- Full inventory slots
@@ -64,12 +70,12 @@ local function countInventory()
   inventory.empty = 0                           -- Reset empty counter
   inventory.full = 0                            -- Reset full counter
   for i = 1,16 do
-    inventory.list[i] = turtle.getItemCount(i)
-    inventory.left[i] = turtle.getItemSpace(i)
-    if inventory.list[i] == 0 then             -- If the slot is empty, add one to the empty counter.
+    inventory.count[i] = turtle.getItemCount(i)
+    inventory.space[i] = turtle.getItemSpace(i)
+    if inventory.count[i] == 0 then             -- If the slot is empty, add one to the empty counter.
       inventory.empty = inventory.empty + 1
     end
-    if inventory.left[i] == 0 then             -- If the slot is full, add one to the full counter.
+    if inventory.space[i] == 0 then             -- If the slot is full, add one to the full counter.
       inventory.full = inventory.full + 1
     end
   end
@@ -97,75 +103,92 @@ end
 
 -- Turning
 -- Function to turn the turtle in a relative direction.
-local function turtleTurn(direction)          -- Relative direction (Right/Left)
-  if direction ~= "Left" or direction ~= "Right" then
+local function turtleTurn(dir)          -- Relative direction (right/left)
+  local dirValues = {
+    left = turtle.turnLeft,
+    right = turtle.turnRight
+  }
+
+  local validInput = { left = true, right = true }
+
+  dir = string.lower(dir)
+
+  if not validInput[dir] then
     writeMessage("(turtleTurn): Bad argument. Direction not recognized. Got "..direction, messageLevel.FATAL)
     return(false)                             -- Returns false as input was not correct.
   end
-  if direction == "Left" then
-    facing = facing - 1                         -- Subtracts 1 from facing
-    turtle.turnLeft()
-  elseif direction == "Right" then
-    facing = facing + 1                         -- Adds 1 to facing
-    turtle.turnRight()
-  end
-  writeMessage("turtleTurn): Facing before fmod: "..facing, messageLevel.DEBUG)
-  facing = math.fmod(facing, 4)                 -- Wraps facing around'
-  writeMessage("turtleTurn): Facing after fmod: "..facing, messageLevel.DEBUG)
-  return(facing)
+
+  facing = "left" and facing - 1 or facing + 1  -- Adjusts facing
+  facing = math.fmod(facing, 4)                 -- Wraps facing around
+  dir()   -- Turns turtle in the direction specified by the argument
+  return(facing)    -- Returns the facing as turning is always successful if the input is correct
 end
 
---[[ turtleFace()
+-- turtleFace()
 -- Function to turn the turtle in a numerical compass direction using the least amount of turns.
-local f#unction turtleFace(dir)                -- Numeric direction
+local function turtleFace(dir)                -- Numeric direction
   if facing - dir > 0 then                     -- Shorter to turn left
     repeat
-      turtleTurn("Left")
+      turtleTurn("left")
     until facing == direction                  -- Turns until correct facing
   elseif facing - dir < 0 then                 -- Shorter to turn right
     repeat
-      turtleTurn("Right")
+      turtleTurn("right")
     until facing == direction                  -- Turns until correct facing
   end
   return(true)                                  -- Returns true as turning is always possible
 end
---]]
 
 -- Moving
--- Function to move the turtle in a numerical compass direction. Will use turtleFace to make it move in that specific direction.
-local function turtleMove(direction)            -- Numeric direction.
-  if direction == nil then
-    return(turtle.forward())                      -- Commands turtle to move forward and returns boolean success.
-  elseif direction == 4 or direction == 5 then
-    return(turtle[directions[direction]])
-  end
-end
+-- Function to move the turtle in a relative direction.
+local function turtleMove(dir)  -- Dir must be a string
+  local dirValues = {           -- Functions for each valid input
+    forward = turtle.forward,
+    back = turtle.back,
+    up = turtle.up,
+    down = turtle.down
+    }
 
--- Block manipulation
--- Tries to place a block in the direction specified. If no direction is specified, block is placed in front of the turtle.
--- Returns boolean value of success.
-local function placeBlock(dir)
-  local success = false
-  if dir == nil then                           -- If direction is not specified, use forward facing.
-    if turtle.place() then
-      success = true
-    end
-  elseif dir == "Up" then
-    if turtle.placeUp() then
-      success = true
-    end
-  elseif dir == "Down" then
-    if turtle.placeDown() then
-      success = true
-    end
-  else
-    writeMessage("(placeBlock): Bad argument. Direction not recognized. Got "..dir, messageLevel.FATAL)
+  -- Valid inputs
+  local validInput = { forward = true, back = true, up = true, down = true }
+
+  dir = string.lower(dir)
+
+  -- Check to see if the input is valid
+  if not validInput[dir] then
+    writeMessage("(turtleMove): Bad argument. Direction not recognized. Got: "..dir, messageLevel.FATAL)
     return(false)
   end
 
-  if success == true then
-    inventory.list[inventory.selected] = inventory.list[inventory.selected] - 1
-    inventory.left[inventory.selected] = inventory.left[inventory.selected] + 1
+  -- Return movement boolean
+  return(dirValues[dir]())
+end
+
+-- Block manipulation
+-- Tries to place a block in the direction specified.
+-- Returns boolean value of success.
+local function placeBlock(dir)  -- Dir must be a string
+  local dirValues = {         -- Functions for each valid input
+    forward = turtle.place,
+    up = turtle.placeUp,
+    down = turtle.placeDown
+  }
+
+  -- Valid inputs
+  local validInput = { forward = true, up = true, down = true }
+
+  dir = string.lower(dir)
+
+  -- Check to see if the input is valid
+  if not validInput[dir] then
+    writeMessage("(placeBlock): Bad argument. Direction not recognized. Got: "..dir, messageLevel.FATAL)
+    return(false)
+  end
+
+  -- Try to place a block. If true, update inventory.
+  if dirValues[dir]() then
+    inventory.count[inventory.selected] = inventory.count[inventory.selected] - 1
+    inventory.space[inventory.selected] = inventory.space[inventory.selected] + 1
     return(true)
   else
     return(false)
@@ -191,11 +214,6 @@ local function detectScrapBlock(dir)
     end
   end
   return(compare)
-end
-
--- Mine the block in the specified direction
-local function mineBlock(dir)
-  return(turtle.dig[dir]())
 end
 
 -- **********************************************************************************
@@ -243,29 +261,32 @@ end
 -- Program
 -- **********************************************************************************
 
--- Debugger is not able to run when instructions are given. Remove comment tags to run the program in Minecraft.
+--[[ Debugger is not able to run when instructions are given. Remove comment tags to run the program in Minecraft.
 -- Open network side
 rednet.open("right")
--- Set os.pullEvent to Raw.
-os.pullEvent = os.pullEventRaw()
+-- Remove bad functions
+local os.pullEvent = os.pullEventRaw
+local oldTurtleSelect = turtle.select
+local turtle.select = selectInventorySlot
 -- Load last state from file
 -- Run self checks
   -- Get GPS location
-  local pos1 = vector.new(gps.locate(2, false))
-  while not turtle.forward do
+  local pos1 = vector.new(gps.locate(2, false))   -- Find first position
+  while not turtle.forward do       -- Move forward one block or up until forward is possible.
     turtle.up()
   end
-  position = vector.new(gps.locate(2, false))
-  local pos2 = position.x, position.y, position.z
-  local vector = pos2 - pos1
+  position = vector.new(gps.locate(2, false))   -- Get new position and add that to position table
+  local pos2 = position.x, position.y, position.z   -- Make a new value to do math with
+  local vector = pos2 - pos1  -- Find difference between position one and two
+
   if vector.x > 0 then
-    facing = 0
+    facing = 0  -- North
   elseif vector.x < 0 then
-    facing = 2
+    facing = 2   -- South
   elseif vector.y > 0 then
-    facing = 1
+    facing = 1   -- East
   elseif vector.y < 0 then
-    facing = 3
+    facing = 3    -- West
   end
 -- Update inventory
   countInventory()
@@ -277,5 +298,15 @@ paralell.waitForAll(mainLoop(), eventLoop())
 
 -- End program
 --]]
+local function testFunction(variable)
+  print(variable)
+  return(true)
+end
 
-writeMessage("Test message", messageLevel.DEBUG)
+turtle = { forward = testFunction, place = testFunction, placeUp = testFunction, placeDown = testFunction, select = testFunction }
+rednet = { broadcast = testFunction }
+selectInventorySlot(1)
+table.insert(inventory.count, 1, 5)
+table.insert(inventory.space, 1, 59)
+print("Inventory.list: "..inventory.count[1])
+print("Inventory.left: "..inventory.space[1])
